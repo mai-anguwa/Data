dddda0b0f44a60af618d313470ba66f700797a76
mai-anguwa <110782823+mai-anguwa@users.noreply.github.com>
2022-10-15 17:41:50 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,106 @@
+ 
+ window.onload = function(){
+setInterval(
+function data(){
+  var amount = Math.floor(Math.random()*990);
+  
+var send = Math.random()*99999990;
+ let sender = Math.floor(send)
+var tim = new Date();
+var time = tim.getMinutes();
+var tie = time + ":" + tim.getSeconds();
+
+document.getElementById('amount').innerHTML = amount.toLocaleString()+"GB";
+document.getElementById('sender').innerHTML = "+" +sender;
+document.getElementById('time').innerHTML =  tie;
+if(amount>10000){
+ alert("Sender : " + sender + " Amount : "+ amount);
+}
+},100);
+
+// end transaction
+   // var bln = Number(new Date().getMinutes())
+   var bln = Math.floor(Math.random()*999)
+   var bl = bln*999;
+   document.getElementById("bal").value = "$ "+ bl.toLocaleString();
+  sessionStorage.setItem("bln",bl)
+ }
+ function send(){
+    var bank = document.getElementById("bankn").value.toUpperCase();
+     if(document.getElementById("acn").value.length < 10){
+       var notifi = document.getElementById("not");
+       notifi.innerHTML = "This Is Not " +bank+ " Account Number, Check !"
+       notifi.style.display = "block";
+      setTimeout(function(){notifi.style.display = "none";},5000) 
+     }else{
+      var balan = Number(document.getElementById("bal").value);
+      var amount = document.getElementById("amount").value;
+      sessionStorage.setItem("amount",amount)
+      // here
+      var pinc = "<div class='pind'><input type='number' class='k' id='pine' placeholder='ENTER PIN'><button class='send' onclick='check()'>Send</button></div>"
+      document.getElementById("render").innerHTML = pinc;
+     }
+ }
+ function check(){
+   var notif = document.getElementById("not"); 
+  if(!document.getElementById("pine").value.length < 4){
+  var amount = Number(sessionStorage.amount);
+  var balan = Number(sessionStorage.bln);
+  var nml = balan - amount;
+  sessionStorage.setItem("bln",nml)
+  document.getElementById("bal").value = "$ "+ nml.toLocaleString();
+
+// here goes the change
+
+  document.getElementById("render").innerHTML = ""
+ setTimeout(function(){
+   notif.innerHTML ="Transfered Successfully!";
+   notif.style.display = "block"
+   notif.style.display = "block";
+    setTimeout(function(){notif.style.display = "none";},5000);
+  },2000);
+  }else{
+    notif.innerHTML = "Incorrect Pin!";
+    notif.style.display = "block";
+    setTimeout(function(){notif.style.display = "none";},5000); 
+  }
+ }
+function close(){
+  document.getElementById("render").innerHTML = " "
+  console.log("closed")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
